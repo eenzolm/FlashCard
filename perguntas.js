a79c141a1e5209f1462fb9700d61284068acf48f
eenzolm <enzoantoniopr@gmail.com>
2024-11-06 11:11:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Programação',
+    'O que é alvinegro praiano?',
+    'No dia 23 de fevereiro de 1976, o Santos FC sofria a segunda alteração em sua denominação passando a ter o seu nome escrito em português ficando Santos Futebol Clube, com a aprovação do Conselho Deliberativo na ocasião presidido por Modesto Roma.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é os maiores jogadores da história do Santos FC?',
+    'Pelé,Neymar Jr,Pepe,zito,Léo'
+)
+criaCartao(
+    'Programação',
+    'Qual é o maior artilheiro do Santos Pós Pelé',
+    'Pepe (canhão da vila) com 403 Gols'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Qual foi o último titúlo do Santos FC?',
+    'Foi um Paulistão em 2016, contra o Grêmio Osasco Audax,ganhando a final por 2x1 (ag)'
+)
